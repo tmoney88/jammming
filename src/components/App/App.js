3c1682eb19c0a01395fc8be8eb13ca9fbaac339c
Tanner Gates <tgates4@dmail.dixie.edu>
2020-03-07 16:30:23 -0700

    Add search and handleTermChange methods to SearchBar

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -27,6 +27,7 @@ class App extends Component {
             this.removeTrack = this.removeTrack.bind(this);
             this.updatePlaylistName = this.updatePlaylistName.bind(this);
             this.savePlaylist = this.savePlaylist.bind(this);
+            this.search = this.search.bind(this);
     }
 
     addTrack(track) {
@@ -53,13 +54,17 @@ class App extends Component {
         const trackURIs = this.state.playlistTracks.map(track => track.uri);
     }
 
+    search(term) {
+        console.log(term);
+    }
+
     render() {
 
         return (
             <div>
                 <h1>Ja<span className="highlight">mmm</span>ing</h1>
                 <div className="App">
-                    <SearchBar />
+                    <SearchBar onSearch={this.search} />
                     <div className="App-playlist">
                         <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack} />
                         <PlayList playlistName={this.state.playlistName} 
