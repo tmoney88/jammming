e802fb0f555f9880065c97f47b1a275efddd3595
Tanner Gates <tgates4@dmail.dixie.edu>
2020-03-08 12:10:48 -0600

    Create save to playlist method in App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -11,17 +11,11 @@ class App extends Component {
 
         this.state = {
             searchResults: [
-                            {name: 'name1', artist: 'artist1', album: 'album1', id: 1},
-                            {name: 'name2', artist: 'artist2', album: 'album2', id: 2},
-                            {name: 'name3', artist: 'artist3', album: 'album3', id: 3},
-                            {name: 'name4', artist: 'artist4', album: 'album4', id: 4}
+                            
                         ],
-            playlistName: 'tmoney playlist',
+            playlistName: 'My playlist',
             playlistTracks: [
-                            {name: 'name1', artist: 'artist1', album: 'album1', id: 5},
-                            {name: 'name2', artist: 'artist2', album: 'album2', id: 6},
-                            {name: 'name3', artist: 'artist3', album: 'album3', id: 7},
-                            {name: 'name4', artist: 'artist4', album: 'album4', id: 8}
+                            
                         ]
             };
             this.addTrack = this.addTrack.bind(this);
@@ -52,7 +46,13 @@ class App extends Component {
     }
 
     savePlaylist() {
-        const trackURIs = this.state.playlistTracks.map(track => track.uri);
+        const trackUris = this.state.playlistTracks.map(track => track.uri);
+        Spotify.savePlaylist(this.state.playlistName, trackUris).then(() => {
+            this.setState({
+                playlistName: 'New Playlist',
+                playlistTracks: []
+            })
+        })
     }
 
     search(term) {
